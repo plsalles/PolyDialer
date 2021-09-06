2f8a69fe1ae0fa60b9a50f1caeec19d19fb3e260
Paulo Salles <plsalles@gmail.com>
2021-09-06 17:24:45 -0300

    Implemented Dialer with https, pending import the function dialer to import csv file and make multiple calls at the same time

--- app.js
+++ app.js
@@ -1,214 +1,210 @@
 'use strict'
 
 //////////Importing libraries/////////
-const fs = require('fs');
-var report = "";
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 /////////////////////////////////////
 
-//////////////VARIABLES///////////////
-let concurrentCalls = 2;
-//////////////////////////////////////
-
-/////////////////////////////////Call Controller/////////////////////////////////////////////////////////
-const Telnet = require('telnet-client')
-
-let regexInCall = /inacall online/;
-
-class Calls {
-  constructor() {
-    this.calls = []
-    this.index = 0
-  }
-
-  // create a new player and save it in the collection
-
-  allCalls() {
-    return this.calls
-  }
-
-  incrementIndex(i) {
-    this.index += i
-  }
-
-  getIndex() {
-    return this.index
-  }
-
-  newCall(name, ip, password) {
-    let c = new Call(name, ip, password)
-    this.calls.push(c)
-    return c
-  }
-  // this could include summary stats like average score, etc. For simplicy, just the count for now
-  numberOfCalls() {
-    return this.calls.length
-  }
-}
 
 class Call {
-  constructor(name, ip, password) {
-    this.name = name;
-    this.ip = ip;
-    this.password = password;
-    this.state = "disconnected";
-  }
-  async call() {
-    console.log(`Calling Endpoint ${this.name} - IP: ${this.ip}`)
-    let connection = new Telnet()
-
-    //Telnet parameters
-    let params = {
-      failedLoginMatch: '-- password failed, retry --',
-      host: this.ip,
-      port: 24,
-      password: `${this.password}\r\n`,
-      passwordPrompt: 'Password: ',
-      shellPrompt: '-> ',
-      timeout: 8000
+    constructor(name, ip, password) {
+        this.name = name;
+        this.ip = ip;
+        this.password = password;
+        this.session = ""
+        this.sessionId = "";
+        this.state = "";
+        this.connectionId = 0;
+        this.error = "";
     }
 
-    try {
-      await connection.connect(params)
-      console.log(`Connected to the GS Endpoint IP: ${this.ip} Name: ${this.name}`)
-    } catch (error) {
-      console.log(`Failed to connect to the GS Endpoint IP: ${this.ip} Name: ${this.name}`)
-      console.log(error)
-      return error
+    async getMediaStat() {
+        const axios = require('axios');
+        let res = "";
+        let mediaStat = await axios.get(`https://192.168.1.133/rest/conferences/0/connections/${this.connectionId}/mediastat`, {
+            headers: {
+                Cookie: `session_id=${this.sessionId}`
+            }
+        })
+        console.log(mediaStat.data)
+    }
+
+    async getSession() {
+        const axios = require('axios');
+        let res = "";
+
+        if (this.sessionId === "") {
+            try {
+                res = await axios.post(`https://${this.ip}/rest/session`, {
+                    "action": "Login",
+                    "user": "admin",
+                    "password": this.password
+                })
+                console.log("GetSession Line 42")
+                console.log("POST --> ", res.data.session)
+                this.session = res.data.session;
+                this.sessionId = res.data.session.sessionId;
+
+            } catch (error) {
+
+                if (error.code) {
+                    console.log("Error --> ", error.code)
+                    this.error = error.code
+                    return
+                }
+
+                if (error.response.status) {
+                    console.log("Error --> ", error.response.status)
+                    console.log("Reason --> ", error.response.data.reason)
+                    this.error = error.response.data.reason;
+                    return
+                }
+
+                console.log(error)
+                return
+            }
+        }
+
+        try {
+            res = await axios.get('https://192.168.1.133/rest/session', {
+                headers: {
+                    Cookie: `session_id=${this.sessionId}`
+                }
+            })
+            console.log("GET --> ", res.data)
+        } catch (error) {
+            console.log(error.config)
+            console.log(error.response)
+        }
+    }
+
+    async makeCall() {
+        const axios = require('axios');
+        let res = "";
+        let currentStatus = await axios.get('https://192.168.1.133/rest/conferences/active', {
+            headers: {
+                Cookie: `session_id=${this.sessionId}`
+            }
+        })
+        console.log(currentStatus.data.connections)
+
+        if (currentStatus.data.connections.length === 0) {
+            try {
+                res = await axios.post(`https://${this.ip}/rest/conferences`, {
+                    "address": "888888.1120324757@t.plcm.vc",
+                    "dialType": "H323",
+                    "rate": "1024"
+                }, {
+                    headers: {
+                        Cookie: `session_id=${this.sessionId}`
+                    }
+                })
+                this.connectionId = res.data[0].href.split("/")[5];
+                console.log("ConnectionID --> ", res.data[0].href.split("/")[5])
+                currentStatus = await axios.get('https://192.168.1.133/rest/conferences/active', {
+                    headers: {
+                        Cookie: `session_id=${this.sessionId}`
+                    }
+                }).then("System in call", console.log(currentStatus.data.connections))
+
+
+            } catch (error) {
+                console.log(error.response.status)
+                console.log(error.response.data.reason)
+            }
+        } else {
+            return ("The system is already in a call", currentStatus.data)
+        }
+    }
+
+    async terminateCall() {
+        const axios = require('axios');
+        let res = "";
+        let currentStatus = await axios.get('https://192.168.1.133/rest/conferences/active', {
+            headers: {
+                Cookie: `session_id=${this.sessionId}`
+            }
+        })
+        console.log(this.session)
+        console.log(currentStatus.data)
+
+        if (currentStatus.data.connections.length != 0) {
+            try {
+                res = await axios.post(`https://${this.ip}/rest/conferences/active`, {
+                    "action": "hangup"
+                }, {
+                    headers: {
+                        Cookie: `session_id=${this.sessionId}`
+                    }
+                })
+                this.connectionId = 0
+
+
+
+            } catch (error) {
+                console.log(error.response.status)
+                console.log(error.response.data.reason)
+            }
+
+            console.log("The current call is disconnected", currentStatus.data.connections)
+        } else {
+            console.log("The system is not in a call", currentStatus.data)
+        }
+
+
+
     }
 
 
-    report += `\t${this.name}\t${this.ip}`;
-    let res = '';
-    let i = 0;
-    let regexInCall = /inacall online/;
-
-    res = await connection.send('dial manual 1024 888888.1120324757@t.plcm.vc h323');
-    report += `\t${res.replace(/\r?\n|\r/g, "")}`;
-
-    setTimeout(async function () {
-      let res = await connection.send('status');
-
-      if (regexInCall.test(res)) {
-        report += `\tinacall online`;
-        this.state = "connected";
-        await connection.end();
-        return `GS Endpoint IP: ${this.ip} Name: ${this.name} is connected`
-        await connection.end();
-      } else {
-        report += `\tinacall offline\n`;
-        await connection.end();
-        return `GS Endpoint IP: ${this.ip} Name: ${this.name} did not connect, please check the logs`
-      }
-
-    }, 8000);
-  }
-  
-  async disconnect() {
-    let connection = new Telnet()
 
 
-    let params = {
-      host: this.ip,
-      port: 24,
-      password: `${this.password}\r\n`,
-      passwordPrompt: 'Password: ',
-      shellPrompt: '-> ',
-      timeout: 8000
+    async terminateSession() {
+        const axios = require('axios');
+        let res = "";
+
+        try {
+            console.log("this.sessionId = ", this.sessionId)
+            res = await axios.delete('https://192.168.1.133/rest/sessions/self', {
+                headers: {
+                    Cookie: `session_id=${this.sessionId}`
+                }
+            })
+        } catch (error) {
+            console.log(error.config)
+            console.log(error.response)
+        }
     }
-    try {
-      await connection.connect(params)
-    } catch (error) {
-      return error;
-    }
-    if (this.state = "connected") this.state = "disconnected"
-    let res = await connection.send('hangup all');
-    console.log(`Connected to the GS Endpoint IP: ${this.ip} Name: ${this.name} - Call is disconnected`)
-    await connection.end();
-    return res;
 
-  }
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let newCall = new Call("GS1", "192.168.1.133", "5712");
 
-console.log("GSDialer initialized");
-
-console.log("Reading CSV file")
-const { promises: { readFile } } = require("fs");
-const { clear } = require('console');
-
-// Promise.all([promise1, promise2, promise3]).then((values) => {
-//   console.log(values);
-// });
-readFile("/mnt/d/Repo/GS-ReportStatus-Telnet/GroupSeriesList.txt").then(fileBuffer => {
-  let input = fileBuffer.toString().split('\r\n')
-  console.log(input)
-  input = input.forEach((element)=> {
-    let output = element.split(',')
-    console.log(output)
-  })
-  
-
-
-  /////////Converting the CSV input to a Object////////////
-  const [headerLine, ...lines] = fileBuffer.toString().split('\r\n');
-  const valueSeparator = ',';
-  const headers = headerLine.split(valueSeparator);
-  const objects = lines
-    .map((line, index) =>
-      line.split(valueSeparator).reduce(
-        (object, value, index) => ({
-          ...object,
-          [headers[index]]: value,
-        }),
-        {}
-      )
-    );
-  ///////////////////////////////////////////////////////
-
-  let calls = new Calls();
-  objects.forEach((element) => {
-    calls.newCall(element.endpoint_name, element.ip_address, element.password)
-  })
-
-  let dialerInterval = setInterval(async function () {
-    if (calls.allCalls().length <= calls.getIndex()) {
-      clearInterval(dialerInterval)
-      setTimeout(() => { console.log("GS Dialer has finished all calls, please check the report") }, 5000)
+newCall.getSession().then(() => {
+    if (newCall.sessionId != "") {
+        newCall.makeCall().then(() => {
+            if(newCall.connectionId != 0){
+                setTimeout(() => {
+                    newCall.getMediaStat().then(() => {
+                        newCall.terminateCall().then(() => {
+                            newCall.terminateSession();
+                        });
+                    });
+                }, 65000)
+            } else {
+                console.log("System already in a call")
+            }
+        })
     } else {
-      dialer(calls, concurrentCalls);
+        switch (newCall.error) {
+            case 'ECONNREFUSED':
+                console.log("ERROR --> Unable to connect to the endpoint, please check the IP and try again")
+                break
+            case 'SessionInvalidUserNamePassword':
+                console.log("ERROR --> Invalid username or password")
+                break    
+            default:
+                console.log("ERROR --> An unexpected error happened!")
+        }
     }
-  }, 15000);
-}).catch(error => {
-  console.log("entrou aqui")
-  console.error(error.message);
-  process.exit(1);
 })
 
-
-
-// Support Functions
-
-function dialer(calls, concurrentCalls) {
-
-  let start = calls.getIndex()
-  let stop;
-  if (start + concurrentCalls > calls.allCalls().length) {
-    stop = calls.allCalls().length
-    calls.incrementIndex(calls.allCalls().length - start)
-  } else {
-    stop = start + concurrentCalls
-    calls.incrementIndex(concurrentCalls)
-  }
-
-  for (let i = start; i < stop; i++) {
-    let newCall = calls.allCalls()[i];
-    let resCall = newCall.call()
-
-    if (resCall.toString().indexOf('Error:') === -1) {
-      setTimeout(async function () {
-        let resDisconnect = newCall.disconnect();
-      }, 20000);
-    }
-  }
-}
